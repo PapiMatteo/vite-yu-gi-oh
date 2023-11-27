6243b7152afde6bc1f6db610aa3167c086f334a1
PapiMatteo <matteopapi.work@gmail.com>
2023-11-27 14:48:13 +0100

    add: AppHeader and AppContent components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <script>
+import axios from "axios";
 import AppHeader from './components/AppHeader.vue';
+import {store} from './store.js';
+import AppContent from "./components/AppContent.vue";
+
+
 
 export default {
     data() {
-        return {};
+        return {
+          store,
+        };
     },
-    components: { AppHeader }
+    created() {
+      this.store.loading = true;
+      axios
+        .get(this.store.apiUrl)
+        .then((resp) => {
+          this.store.cardsList = resp.data
+        })
+    },
+    components: { AppHeader, AppContent }
 }
 
 </script>
 
 <template>
   <AppHeader/>
-
+  <AppContent/>
+  
 </template>
 
 <style lang="scss">
---
+++ src/components/AppContent.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="wrapper mt-5">
+        <div class="container pt-5">
+            <div class="content-top "></div>
+            <div class="row row-col-5">
+                <div class="col">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.wrapper {
+    width: 80%;
+    margin: 0 auto;
+    background-color: white;
+
+    .container {
+        .content-top {
+            background-color: #212529;
+            color: white;
+            height: 70px;
+        }
+    }
+}
+
+</style>
