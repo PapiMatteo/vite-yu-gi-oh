c507af641d4bbcd0076ce27fd53abedea64990bf
PapiMatteo <matteopapi.work@gmail.com>
2023-11-27 14:18:10 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+header {
+    height: 80px;
+    background-color: white;
+    
+    h1 {
+        padding-top: 10px;
+    }
+}
+
+</style>
